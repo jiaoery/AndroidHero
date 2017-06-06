c7e52b90d460f107894dfe6a4a8ab7ceff88caf7
cqjix <373105324@qq.com>
2017-06-06 11:38:23 +0800

    第三章内容 学习自定义view GradientTextView 实现渐变效果的文字效果

---
+++ app/src/main/java/androidhero/com/androidhero/chapter3/TestGradientActivity.java
@@ -0,0 +1,15 @@
+package androidhero.com.androidhero.chapter3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidhero.com.androidhero.R;
+
+public class TestGradientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_gradient);
+    }
+}
--- app/src/main/java/androidhero/com/androidhero/widget/GranientTextView.java
+++ app/src/main/java/androidhero/com/androidhero/widget/GranientTextView.java
@@ -1,5 +1,15 @@
 package androidhero.com.androidhero.widget;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.support.annotation.PluralsRes;
+import android.util.AttributeSet;
 import android.widget.TextView;
 
 /**
@@ -7,5 +17,54 @@ import android.widget.TextView;
  * 渐变的textview
  */
 
-//public class GranientTextView extends TextView{
-//}
+public class GranientTextView extends TextView {
+    private int mViewWidth = 0;
+    private Paint mPaint;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private int mTranslate;
+
+    public GranientTextView(Context context) {
+        super(context);
+    }
+
+    public GranientTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GranientTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public GranientTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0XFFFFFF, Color.BLUE}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(mGradientMatrix!=null){
+            mTranslate+=mViewWidth/5;
+            if(mTranslate>2*mViewWidth){
+                mTranslate=-mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
